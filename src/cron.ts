90f8b16df406de89662554df777f07100813fea2
slavapopov <vyacheslav.popov@fabit.ru>
2026-01-22 17:33:33 +0500

    change type

--- src/cron.ts
+++ src/cron.ts
@@ -4,7 +4,6 @@ import { bot } from './botInstance';
 
 const CHANNEL_ID = process.env.CHANNEL_ID!;
 
-// Каждый день в 09:00
 cron.schedule('0 9 * * *', async () => {
   const digest = await getNewsDigestEmoji();
   await bot.api.sendMessage(CHANNEL_ID, digest, { parse_mode: 'HTML' });
