8ea7d0cbac3ab264022d83cc94b8b2206e4b3c5c
slavapopov <vyacheslav.popov@fabit.ru>
2026-01-23 15:33:30 +0500

    fix 2

--- src/cron/dailyNews.ts
+++ src/cron/dailyNews.ts
@@ -1,21 +1,15 @@
 import cron from 'node-cron';
 import { getNewsDigestEmoji } from '../news/news.js';
 import { bot } from '../botInstance.js';
-import { generateDigestImage } from '../news/imageGemerator.js';
 
 export function setupDailyNewsCron() {
-  cron.schedule('20 9,15,17,21 * * *', async () => {
+  cron.schedule('40 9,15,17,21 * * *', async () => {
     console.log('CRON TICK', new Date().toISOString());
 
     try {
       const digest = await getNewsDigestEmoji();
-      const imageUrl = await generateDigestImage(digest.text);
 
-      console.log('digest', digest);
-      console.log('imageUrl');
-
-      await bot.api.sendPhoto(process.env.CHANNEL_ID!, imageUrl, {
-        caption: digest.text,
+      await bot.api.sendMessage(process.env.CHANNEL_ID!, digest.text, {
         parse_mode: 'HTML',
       });
     } catch (err) {
--- src/news/news.ts
+++ src/news/news.ts
@@ -1,10 +1,10 @@
 import { fetchOpenRouter } from '../openrouter.js';
 import { formatDigest } from './formatter.js';
-import { NEWS_PROMPT } from '../config/prompts.js';
+import { NEWS_DIGEST_EMOJI } from '../config/prompts.js';
 
 export async function getNewsDigestEmoji(): Promise<{
   text: string;
 }> {
-  const raw = await fetchOpenRouter(NEWS_PROMPT);
+  const raw = await fetchOpenRouter(NEWS_DIGEST_EMOJI);
   return formatDigest(raw);
 }
