fd6f92a4f3cefc1f56e409a51430ce35138c43d7
slavapopov <vyacheslav.popov@fabit.ru>
2026-02-20 21:11:46 +0500

    fix

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -8,8 +8,12 @@ module.exports = {
         TELEGRAM_TOKEN: process.env.TELEGRAM_TOKEN,
         OPENAI_API_KEY: process.env.OPENAI_API_KEY,
         GEMINI_API_KEY: process.env.GEMINI_API_KEY,
+        DEEPSEEK_API_KEY: process.env.DEEPSEEK_API_KEY,
         OPENROUTER_API_KEY: process.env.OPENROUTER_API_KEY,
-        CHANNEL_ID: process.env.CHANNEL_ID
+        GROQ_API_KEY: process.env.GROQ_API_KEY,
+        HF_TOKEN: process.env.HF_TOKEN,
+        CHANNEL_ID: process.env.CHANNEL_ID,
+        EVENTS_CHANNEL_ID: process.env.EVENTS_CHANNEL_ID,
       }
     }
   ]
